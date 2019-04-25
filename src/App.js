9ecf52668a111c8490521942dfd1779c7572c112
Alfian Pramudita <alfian.pramudita@gmail.com>
2019-04-25 14:42:03 +0700

    Add hello text

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Hello Everyone !!!
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
